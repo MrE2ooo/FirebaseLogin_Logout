9a78a6743f5c46f5664a36126160354f8d008d99
MrE2000 <eliasymadani@gmail.com>
2024-12-20 20:28:21 +0300

    signUserOut

--- lib/widgets/signinviewbody.dart
+++ lib/widgets/signinviewbody.dart
@@ -1,9 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firelogin/widgets/ErrorDialog.dart';
 import 'package:flutter/material.dart';
 
-class SignInViewBody extends StatelessWidget {
+class SignInViewBody extends StatefulWidget {
   const SignInViewBody({super.key});
 
+  @override
+  State<SignInViewBody> createState() => _SignInViewBodyState();
+}
+
+class _SignInViewBodyState extends State<SignInViewBody> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -12,12 +18,31 @@ class SignInViewBody extends StatelessWidget {
     final TextEditingController passwordController = TextEditingController();
 
     void signUserUp() async {
-      
-       
-       UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+      // show loading circle
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      );
+
+      // sign in impl
+      try {
+        UserCredential userCredential =
+            await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: emailController.text,
           password: passwordController.text,
         );
+      } on FirebaseAuthException catch (e) {
+        print(e);
+        
+      }
+
+      //pop the circle
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context);
     }
 
     return Scaffold(
@@ -27,7 +52,7 @@ class SignInViewBody extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              'SignIn Account',
+              'Sign In Account',
               style: TextStyle(
                 fontSize: 30, // Adjust the font size as needed
                 fontWeight: FontWeight.bold,
@@ -65,9 +90,11 @@ class SignInViewBody extends StatelessWidget {
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.blue, // Background color
-                padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 32.0, vertical: 16.0),
               ),
-              child: const Text('Sign In', style: TextStyle(color: Colors.white)),
+              child:
+                  const Text('Sign In', style: TextStyle(color: Colors.white)),
             ),
             const SizedBox(height: 16.0),
             Row(
